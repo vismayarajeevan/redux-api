0eaf5e221b90cd1d40c02bee4412109bd516c394
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-04-24 20:56:05 +0530

    slice created

---
+++ src/redux/slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const crudSlice = createSlice({
+    name:'crud',
+    initialState:{
+
+    },
+    reducers:{
+
+    }
+})
+
+export default createSlice.reducers
